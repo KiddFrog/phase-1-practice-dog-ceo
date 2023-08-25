6365d1ab11ef24654a229641c13e1cd763498d7a
domm <dommferger@gmail.com>
2023-08-25 15:56:19 -0400

    Part One: DEMOLISHED

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 console.log('%c HI', 'color: firebrick')
+/// Starting MY code
+
+const init = (e) =>{
+        e.preventDefault();
+        //starting fetch
+        fetch("https://dog.ceo/api/breeds/image/random/4")
+            .then((response) => response.json())
+            .then((data) =>{
+                
+                const imgUrl = data.message
+                imgUrl.forEach(message => {
+                                            
+                    const imageContainer = document.querySelector("#dog-image-container");
+                    createImage = document.createElement('img');
+                    imageContainer.appendChild(createImage);
+                    createImage.src = message;
+                        
+                });
+            })
+};
+
+document.addEventListener("DOMContentLoaded", init);
+try {
+        nonExistentFunction();
+      } catch (error) {
+        console.error(error);
+        // Expected output: ReferenceError: nonExistentFunction is not defined
+        // (Note: the exact output may be browser-dependent)
+      }
+      
